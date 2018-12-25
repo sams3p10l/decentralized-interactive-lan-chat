28f8cc0b20f8b8cd53b87d86724603be2dbfe526
sams3p10l <mix996@gmail.com>
2018-12-25 21:01:09 +0100

    datagram parsing done

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,6 +1,8 @@
 #include "connection.h"
 #include <QtNetwork>
 
+static const int TransferTimeout = 30 * 1000;
+
 Connection::Connection(QObject *parent) : QTcpSocket (parent), writer(this)
 {
     incomingConnectionUsername = "unknown";
@@ -19,18 +21,7 @@ Connection::Connection(qintptr handle, QObject *parent) : Connection(parent)
 Connection::~Connection()
 {
     writer.endArray();
-}
-
-bool Connection::sendMessage(const QString &message)
-{
-    if(message.isEmpty())
-        return false;
-
-    writer.startMap(1);
-    writer.append(Message);
-    writer.append(message);
-    writer.endMap();
-    return true;
+    waitForBytesWritten(3000);
 }
 
 void Connection::timerEvent(QTimerEvent *timerEvent)
@@ -55,7 +46,7 @@ void Connection::processData()
     readBuffer.clear();
 }
 
-QString Connection::getIncomingConnectionUsername() const //mozda nepotrebno
+QString Connection::getIncomingConnectionUsername() const
 {
     return incomingConnectionUsername;
 }
@@ -64,7 +55,72 @@ void Connection::processReadyRead()
 {
     reader.reparse();
 
+    while(reader.lastError() == QCborError::NoError)
+    {
+        if(state == WaitingGreeting)
+        {
+            if(!reader.isArray())
+                break;
 
+            reader.enterContainer();
+            state = ParsingGreeting;
+        }
+        else if (reader.containerDepth() == 1) //message and greeting both have depth 1
+        {
+            if (!reader.hasNext()) //if it's the end of the transmission
+            {
+                reader.leaveContainer();
+                disconnectFromHost();
+                return;
+            }
+
+            reader.enterContainer();
+        }
+        else if (type == Undefined) //deducing data type which comes next
+        {
+            if(!reader.isInteger())
+                break;
+
+            type = DataTypes(reader.toInteger());
+            reader.next();
+        }
+        else //read datagram content
+        {
+            if(reader.isString())
+            {
+                auto r = reader.readString();
+                readBuffer += r.data;
+                if(r.status != QCborStreamReader::EndOfString)
+                    continue;
+            }
+            else if (reader.isNull())
+            {
+                reader.next();
+            }
+            else
+                break;
+
+            reader.leaveContainer();
+
+            if(transferTimerID != -1)
+            {
+                killTimer(transferTimerID);
+                transferTimerID = -1;
+            }
+
+            if (state == ParsingGreeting)
+            {
+                if (type != Greeting)
+                    break;
+                processGreeting();
+            }
+            else
+                processData();
+        }
+    }
+
+    if (transferTimerID != -1 && reader.containerDepth() > 1)
+        transferTimerID = startTimer(TransferTimeout);
 }
 
 void Connection::sendGreeting()
@@ -80,6 +136,18 @@ void Connection::sendGreeting()
 
     if(!reader.device())
         reader.setDevice(this);
+}
+
+bool Connection::sendMessage(const QString &message)
+{
+    if(message.isEmpty())
+        return false;
+
+    writer.startMap(1);
+    writer.append(Message);
+    writer.append(message);
+    writer.endMap();
+    return true;
 }
 
 void Connection::processGreeting()
